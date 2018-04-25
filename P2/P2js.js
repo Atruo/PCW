e4e48145870aea46af5f5e11b0f79a42dbabc2c0
serbanuls <banyuls98@gmail.com>
2018-04-25 13:25:32 +0200

    sistema de login implementado

--- P2/P2js.js
+++ P2/P2js.js
@@ -1,17 +1,53 @@
 function hacerLogin(frm){
 
-  let xhr = new  XMLHttpRequest(),
-    url ='./rest/login/',
-    fd = new FormData(frm);
-   
+	let xhr = new XMLHttpRequest(),
+		url = './rest/login/',
+		fd  = new FormData(frm);
 
-    xhr.open('POST', url, true);
-    xhr.onload=function(){
-      console.log(xhr.responseText);
-    };
-    xhr.send(fd);
-    return false;
+	xhr.open('POST',url,true);
+	xhr.onload = function(){
+		console.log(xhr.responseText);
+		let r = JSON.parse(xhr.responseText);
+		if(r.RESULTADO=='OK'){
+			sessionStorage.setItem('usuario',xhr.responseText);
+			console.log("Sesion Iniciada");
+			location.href = "index.html";
+
+		}else{
+			console.log("Error en el login");
+
+			alert("Error al hacer login");
+			location.reload();
+
+		}
+	};
+	xhr.send(fd);
+
+	return false;
+
+}
+
+function comprobarStorage(){
+
+	var login = sessionStorage.getItem('usuario');
+	
+
+	if(login){
+		console.log('SE QUE ESTOY LOGEADO');
+		
+		document.getElementById('menu_login').innerHTML="";
+		document.getElementById('menu_registro').innerHTML="";
 
 
 
+	}else{
+		console.log('NO ESTOY LOGEADO');
+		document.getElementById('menu_nueva').innerHTML="";
+		document.getElementById('menu_logout').innerHTML="";
+	}
+
+}
+
+function cerrarSesion(){
+	sessionStorage.clear();
 }
