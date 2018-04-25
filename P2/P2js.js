e1c9bdb3beada639399ebfa485b565c339e84f95
serbanuls <banyuls98@gmail.com>
2018-04-25 11:58:09 +0200

    login

--- P2/P2js.js
+++ P2/P2js.js
@@ -1,44 +1,17 @@
-function pedirReceta(){
+function hacerLogin(frm){
 
-	let xhr = new XMLHttpRequest(),
-		url = './rest/receta/?u=6';
+  let xhr = new  XMLHttpRequest(),
+    url ='./rest/login/',
+    fd = new FormData(frm);
+   
 
-	xhr.open('GET',url, true);
-	xhr.onload = function(){
-		console.log(xhr.responseText);
-		let r = JSON.parse(xhr.responseText);
-		console.log(r);
-		
-				mostrarRecetas(r);
-		
-	};
+    xhr.open('POST', url, true);
+    xhr.onload=function(){
+      console.log(xhr.responseText);
+    };
+    xhr.send(fd);
+    return false;
 
-	xhr.send();
 
-}
 
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
 }
