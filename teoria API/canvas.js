96df95291e9a6737a7f48155f57d5b6dc829ac67
serbanuls <banyuls98@gmail.com>
2018-04-30 16:46:27 +0200

    toeria

--- teoria API/canvas.js
+++ teoria API/canvas.js
@@ -1,6 +1,9 @@
+var ancho = 480;
+var alto = 360;
+
 function pintarRectangulo(){
 
-	let cv = document.querySelector('body>section:first-of-type>canvas'),
+	let cv = document.querySelector('#cv01'),
 	ctx = cv.getContext('2d');
 
 
@@ -17,11 +20,107 @@ function pintarRectangulo(){
 	ctx.fillRect(200,50,100,60);
 }
 
+
 function prepararCanvas(){
 
 
 
-		let cv = document.querySelector('body>section:first-of-type>canvas');
-		cv.width = '480';
-		cv.height = '360'
+		let cv = document.querySelector('#cv01'),
+			ctx = cv.getContext('2d');
+
+		cv.width = ancho;
+		cv.height = alto;
+
+		ctx.fillStyle = document.querySelector('#colorRelleno').value;
+		ctx.strokeStyle = document.querySelector('#colorBorde').value;
+
+
+
+}
+
+function pintarRect(){
+
+
+	let cv = document.querySelector('#cv01'),
+	ctx = cv.getContext('2d');
+
+	
+	ctx.lineWidth = 2;
+	
+
+	ctx.strokeRect(40,50,100,75);
+
+}
+
+function rellenarRect(){
+
+	let cv = document.querySelector('#cv01'),
+	ctx = cv.getContext('2d');
+
+}
+
+function rellenarSombra(){
+
+	let cv = document.querySelector('#cv01'),
+	ctx = cv.getContext('2d');
+
+	
+	ctx.shadowOffsetX = 0;
+	ctx.shadowOffsetY = 0;
+	ctx.shadowBlur = 10;
+	ctx.shadowColor = '#362525';
+
+	ctx.fillRect(200,100,100,50);
+}
+
+
+function limpiar(){
+
+	let cv = document.querySelector('#cv01'),
+	ctx = cv.getContext('2d');
+
+	//ctx.clearRect(100,100,150,100);	
+	cv.width = cv.width;
+
+}
+
+function dibujarTexto(){
+
+	let cv = document.querySelector('#cv01'),
+	ctx = cv.getContext('2d');
+
+	ctx.font = 'bold 20px Arial';
+	ctx.textAlign = 'center'; 
+	ctx.shadowOffsetX = 5;
+	ctx.shadowOffsetY = 5;
+	ctx.shadowBlur = 10;
+	ctx.shadowColor = '#B4EC1A';
+
+
+	ctx.fillText('Cine de Barrio la señora murió (Feka newz)',ancho/2,alto/2);
+	ctx.lineWidth = 2;
+	ctx.strokeText('Cine de Barrio la señora murió (Feka newz)',ancho/2,alto/2);
+
+}
+
+function cambiarColorBorde(){
+
+
+	let cv = document.querySelector('#cv01'),
+	ctx = cv.getContext('2d');
+
+	ctx.strokeStyle = document.querySelector('#colorBorde').value;
+
+
+}
+
+function cambiarColorRelleno(){
+
+
+	let cv = document.querySelector('#cv01'),
+	ctx = cv.getContext('2d');
+
+	ctx.fillStyle = document.querySelector('#colorRelleno').value;
+
+
 }
